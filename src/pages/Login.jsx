5816d39f5c60eb7523ef63d41b66f07b2050731d
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-11-27 00:57:07 +0530

    minor ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -46,22 +46,17 @@ const Login = () => {
       <Navigate to="/admin"></Navigate>
     );
   return (
-    <div className="d-flex justify-content-center ">
+    <div className="d-flex justify-content-center">
       <div
-        className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary bg-color-primary d-flex rounded-5"
+        className={`position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary bg-color-primary d-flex rounded-5 ${styles.text}`}
         style={{ marginLeft: "30px" }}
       >
         <div className=" p-lg-5 mx-auto my-5 brand_container">
-          <h1 className={`display-5 fw-bold ${styles.text}`}>
+          <h1 className={`display-5 fw-bold  px-5`}>
             {/* NEERAJ <br /> INDUSTRIES & EXPORT */}
-            Abhishek Industries and Exports
+            GoShopping
           </h1>
-          <h3
-            className="fw-normal text-muted mb-3 bg-color-primary"
-            style={{ fontSize: "1rem" }}
-          >
-            Build anything you want with Trust since 2023.
-          </h3>
+
           <div
             className="d-flex gap-3 justify-content-center fw-bold my-5"
             style={{ fontSize: "2rem" }}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -47,20 +47,15 @@ const Register = () => {
   return (
     <div className="d-flex justify-content-center ">
       <div
-        className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary bg-color-primary d-flex rounded-5"
+        className={`position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary bg-color-primary d-flex rounded-5 ${styles.text}`}
         style={{ marginLeft: "30px" }}
       >
         <div className=" p-lg-5 mx-auto my-5 brand_container">
-          <h1 className={`display-5 fw-bold ${styles.text}`}>
+          <h1 className={`display-5 fw-bold `}>
             {/* NEERAJ <br /> INDUSTRIES & EXPORT */}
-            Abhishek Industries and Exports
+            GoShopping
           </h1>
-          <h3
-            className="fw-normal text-muted mb-3 bg-color-primary"
-            style={{ fontSize: "1rem" }}
-          >
-            Build anything you want with Trust since 2023.
-          </h3>
+
           <div
             className="d-flex gap-3 justify-content-center fw-bold my-5"
             style={{ fontSize: "2rem" }}
@@ -70,7 +65,7 @@ const Register = () => {
         </div>
         <section className="d-flex justify-content-center form_container">
           <form
-            className="d-flex flex-column w-100 rounded-5"
+            className="d-flex flex-column w-100 rounded-5 login_form"
             onSubmit={submitHandler}
           >
             <h1 className="head">Register!</h1>
