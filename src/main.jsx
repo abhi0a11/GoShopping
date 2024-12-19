26a339b1c018ea79555b95a2945ecfade5f2bf87
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-12-19 19:28:58 +0530

    server

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import App from "./App.jsx";
 
 export const cloud_server = "https://api.cloudinary.com/v1_1";
 // export const server = "http://localhost:3000";
-// export const server = "https://goshopping-be.onrender.com";
-export const server = "https://neeraj-industries-be.onrender.com";
+export const server = "https://goshopping-be.onrender.com";
+// export const server = "https://neeraj-industries-be.onrender.com";
 
 export const Context = createContext({
   isAuthenticated: false,
